b41b9c75d034470c30d8945bf1757e92c396b27b
sgbaird <sterling.baird@icloud.com>
2022-05-21 02:15:16 -0600

    convert to proper test file

--- tests/xtal2png_test.py
+++ tests/xtal2png_test.py
@@ -5,16 +5,37 @@ from os import path
 
 from pymatgen.core.structure import Structure
 
-from xtal2png.skeleton import XtalConverter as xc
+from xtal2png.skeleton import XtalConverter
 
 EXAMPLE_CIFS = ["Zn2B2PbO6.cif", "V2NiSe4.cif"]
 S = []
 for cif in EXAMPLE_CIFS:
-    fpath = path.join("data", "external", cif)
+    fpath = path.join("..", "data", "external", cif)
     S.append(Structure.from_file(fpath))
 
-xc.structures_to_arrays(S)
 
-xc.structures_to_arrays([S[0]])
+def test_structures_to_arrays():
+    xc = XtalConverter()
+    data = xc.structures_to_arrays(S)
+    return data
+
+
+def test_structures_to_arrays_single():
+    xc = XtalConverter()
+    data = xc.structures_to_arrays([S[0]])
+    return data
+
+
+def test_xtal2png():
+    xc = XtalConverter()
+    imgs = xc.xtal2png(S, show=False, save=True)
+    return imgs
+
+
+def test_xtal2png_single():
+    xc = XtalConverter()
+    imgs = xc.xtal2png([S[0]], show=False, save=True)
+    return imgs
+
 
 1 + 1
