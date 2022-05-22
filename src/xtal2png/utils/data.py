6da773ba568346ca684fe57c692819b6f67fb41b
sgbaird <sterling.baird@icloud.com>
2022-05-21 23:26:55 -0600

    Create data.py with dummy_structures

---
+++ src/xtal2png/utils/data.py
@@ -0,0 +1,9 @@
+from pymatgen.core.lattice import Lattice
+from pymatgen.core.structure import Structure
+
+coords = [[0, 0, 0], [0.75, 0.5, 0.75]]
+lattice = Lattice.from_parameters(a=3.84, b=3.84, c=3.84, alpha=120, beta=90, gamma=60)
+dummy_structures = [
+    Structure(lattice, ["Si", "Si"], coords),
+    Structure(lattice, ["Ni", "Ni"], coords),
+]
