fe12e83b543d8374818416a0cd4fa563bdcc4df5
sgbaird <sterling.baird@icloud.com>
2022-05-30 21:52:58 -0600

    plot_and_save helper

--- src/xtal2png/utils/plotting.py
+++ src/xtal2png/utils/plotting.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 import plotly.graph_objs as go
+from plotly import offline
 
 
 def matplotlibify(
@@ -52,9 +53,9 @@ def matplotlibify(
     >>> fig.show()
 
     Note the difference between
-    https://user-images.githubusercontent.com/45469701/171044741-35591a2c-dede-4df1-ae47-597bbfdb89cf.png
+    https://user-images.githubusercontent.com/45469701/171044741-35591a2c-dede-4df1-ae47-597bbfdb89cf.png # noqa: E501
     and
-    https://user-images.githubusercontent.com/45469701/171044746-84a0deb0-1e15-40bf-a459-a5a7d3425b20.png,
+    https://user-images.githubusercontent.com/45469701/171044746-84a0deb0-1e15-40bf-a459-a5a7d3425b20.png, # noqa: E501
     which are both static exports of interactive plotly figures.
     """
     font_dict = dict(family="Arial", size=size, color="black")
@@ -104,3 +105,24 @@ def matplotlibify(
         return fig, scale
     else:
         return fig
+
+
+def plot_and_save(fig_path, fig, mpl_kwargs={}, show=False, update_legend=False):
+    if show:
+        offline.plot(fig)
+    fig.write_html(fig_path + ".html")
+    fig.to_json(fig_path + ".json")
+    if update_legend:
+        fig.update_layout(
+            legend=dict(
+                font=dict(size=16),
+                yanchor="bottom",
+                y=0.99,
+                xanchor="right",
+                x=0.99,
+                bgcolor="rgba(0,0,0,0)",
+                # orientation="h",
+            )
+        )
+    fig = matplotlibify(fig, **mpl_kwargs)
+    fig.write_image(fig_path + ".png")
